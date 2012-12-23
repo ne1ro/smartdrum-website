17d018835bb2a744fc8f6e714fd1d3dcfba21afa
Aleksey Kuznetsov <neiro@dev.(none)>
2012-12-23 21:15:07 +0400

    Fix staticpages

--- app/admin/news.rb
+++ app/admin/news.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register News do
-  
+  form do |f|
+  f.inputs do
+    f.input :header
+    f.input :text, :input_html => {:class => "ckeditor"}
+  end
+  f.buttons
+end
 end
--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Product do
-  
+form do |f|
+  f.inputs do
+    f.input :name
+    f.input :description, :input_html => {:class => "ckeditor"}
+    f.input :count
+    f.input :price
+  end
+  f.buttons
+end  
 end
