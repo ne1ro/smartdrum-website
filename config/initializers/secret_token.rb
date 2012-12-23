20c4e6bdd2224dfa0757bc60344881a6449e6640
Aleksey Kuznetsov <neiro@dev.(none)>
2012-12-23 17:26:06 +0400

    Create new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smartdrum::Application.config.secret_token = '01be2ffa187f0ebe288e680b21d88c92059b597a7d4ce6eebfc4282b3d6f65cf88f9b32b798522222ac6bbbde7947be62656934d89124889d6d14cb3bdf02bf4'
